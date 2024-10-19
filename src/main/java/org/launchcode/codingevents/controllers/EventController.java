e172ecfd0eee794ee97b434853ed1409b17070c3
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-10-19 01:26:30 -0500

    CT completed Exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -16,11 +17,18 @@ public class EventController {
 
     // A Static ArrayList that allows access at Class level
     // Static List only exist in the lifetime of the application ==> Once Quit, all data will be lost
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
+
+
 
     @GetMapping
     public String displayAllEvents(Model model) {
         String title = "All Events";
+
+        events.put("Menteaship", "A fun meetup for connecting with mentors");
+        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
 
         model.addAttribute("events", events);
         model.addAttribute("title", title);
@@ -40,7 +48,7 @@ public class EventController {
     // Handles request for the form at http://localhost:8080/events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+//        events.add(eventName);
 
         // Returns a Redirect Response to the root path controller ==> displays output of eventName
         return "redirect:/events";
