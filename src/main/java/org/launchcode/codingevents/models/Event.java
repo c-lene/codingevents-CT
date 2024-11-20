839efdf5cc90fb8efc3d1cd7ce752e1491173491
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-11-19 21:29:03 -0600

    Set up 'One-to-One' relationship with Events and EventDetails

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,9 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,23 +20,20 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
-    private String description;
-
-    @NotBlank(message = "Email is REQUIRED.")
-    @Email(message = "Invalid email. Try again!")
-    private String contactEmail;
-
     @ManyToOne
     @NotNull(message = "Category is REQUIRED.")
     private EventCategory eventCategory;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
+
+
 
     /** CONSTRUCTOR **/
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail= contactEmail;
         this.eventCategory = eventCategory;
         
     }
@@ -51,36 +50,25 @@ public class Event extends AbstractEntity {
         return name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public @Email String getContactEmail() {
-        return contactEmail;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
 
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setContactEmail(@Email String contactEmail) {
-        this.contactEmail = contactEmail;
     }
 
     public void setEventCategory(EventCategory eventCategory) {
         this.eventCategory = eventCategory;
     }
 
-
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
 
     /** ToString Method **/
     @Override
---
+++ src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -0,0 +1,54 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class EventDetails extends AbstractEntity {
+
+    /** FIELDS **/
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @NotBlank(message = "Email is REQUIRED.")
+    @Email(message = "Invalid email. Try again!")
+    private String contactEmail;
+
+
+    public EventDetails(String description, String contactEmail) {
+        this.description = description;
+        this.contactEmail = contactEmail;
+    }
+
+    /** CONSTRUCTORS **/
+
+    // Primary Constructor
+
+
+    // No-args Constructor
+    public EventDetails () {
+
+    }
+
+
+
+    /** GETTER & SETTER **/
+    public @Size(max = 500, message = "Description too long!") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Size(max = 500, message = "Description too long!") String description) {
+        this.description = description;
+    }
+
+    public @NotBlank(message = "Email is REQUIRED.") @Email(message = "Invalid email. Try again!") String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(@NotBlank(message = "Email is REQUIRED.") @Email(message = "Invalid email. Try again!") String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+}
