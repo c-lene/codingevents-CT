5e0750f44b4a0a12d357f117b5b17f18c0e5ade2
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-11-20 13:39:13 -0600

    Added 'Many-to-Many-features' complete

--- src/main/java/org/launchcode/codingevents/models/dto/EventTagDTO.java
+++ src/main/java/org/launchcode/codingevents/models/dto/EventTagDTO.java
@@ -1,4 +1,42 @@
 package org.launchcode.codingevents.models.dto;
 
+import jakarta.validation.constraints.NotNull;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.Tag;
+
 public class EventTagDTO {
+
+    /** FIELDS **/
+    @NotNull
+    private Event event;
+
+    @NotNull
+    private Tag tag;
+
+
+
+    /** CONSTRUCTOR **/
+
+    //No-args Constructor
+    public EventTagDTO() {}
+
+
+
+    /** GETTERS & SETTERS **/
+    public @NotNull Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(@NotNull Event event) {
+        this.event = event;
+    }
+
+
+    public @NotNull Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(@NotNull Tag tag) {
+        this.tag = tag;
+    }
 }
